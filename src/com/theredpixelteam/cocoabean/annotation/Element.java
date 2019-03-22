cd244c97601a921b35054ee2eafc8bc7908c101e
Kumonda221 <kumonda@kucro3.org>
2019-03-23 00:28:32 +0800

    增量更新 20190323

---
+++ src/com/theredpixelteam/cocoabean/annotation/Element.java
@@ -0,0 +1,28 @@
+package com.theredpixelteam.cocoabean.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation of a CocoaBean element in an entity.
+ */
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Element {
+    /**
+     * Represents the identity of this entity.
+     *
+     * @return Identity of entity
+     */
+    public String identity();
+
+    /**
+     * Whether this entity is read-only. This flag will only take
+     * effect when annotating a <b>value-type</b> entity.
+     *
+     * @return Whether read-only, false for default.
+     */
+    public boolean readonly() default false;
+}
---
+++ src/com/theredpixelteam/cocoabean/annotation/Entity.java
@@ -0,0 +1,20 @@
+package com.theredpixelteam.cocoabean.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation of CocoaBean entity.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Entity {
+    /**
+     * Represents the identity of this entity.
+     *
+     * @return Identity of entity
+     */
+    public String identity();
+}

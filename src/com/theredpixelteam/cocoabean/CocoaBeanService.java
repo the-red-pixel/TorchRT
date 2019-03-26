37ec96e803319bf16d176799fd6085f0be1c910d
Kumonda221 <kumonda@kucro3.org>
2019-03-27 05:31:20 +0800

    增量更新 20190327

--- src/com/theredpixelteam/cocoabean/CocoaBeanService.java
+++ src/com/theredpixelteam/cocoabean/CocoaBeanService.java
@@ -1,7 +1,10 @@
 package com.theredpixelteam.cocoabean;
 
+import javax.annotation.Nonnull;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
 
 /**
  * This object represents a CocoaBean service, providing bean
@@ -12,7 +15,38 @@ public class CocoaBeanService {
     {
     }
 
-    // TODO
+    /**
+     * Get namespace.
+     *
+     * @param name Name of the namespace
+     * @return Namespace instance if any
+     */
+    public @Nonnull Optional<CocoaBeanNamespace> getNamespace(@Nonnull String name)
+    {
+        return Optional.ofNullable(namespaces.get(Objects.requireNonNull(name, "name")));
+    }
+
+    /**
+     * Get namespace and create one if absent.
+     *
+     * @param name Name of the namespace
+     * @return Existing or created namespace
+     */
+    public @Nonnull CocoaBeanNamespace getOrCreateNamespace(@Nonnull String name)
+    {
+        return namespaces.computeIfAbsent(Objects.requireNonNull(name, "name"), CocoaBeanNamespace::new);
+    }
+
+    /**
+     * Remove namespace.
+     *
+     * @param name Name of the namespace
+     * @return Whether the specified namespace exists and is removed
+     */
+    public boolean removeNamespace(@Nonnull String name)
+    {
+        return namespaces.remove(Objects.requireNonNull(name, "name")) != null;
+    }
 
     protected Map<String, CocoaBeanNamespace> namespaces = new HashMap<>();
 }

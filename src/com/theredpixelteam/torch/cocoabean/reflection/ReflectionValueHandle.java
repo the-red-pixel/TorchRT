4475c95322ae50a02a6d1186a5aba158909a181a
Kumonda221 <kumonda@kucro3.org>
2019-04-03 00:06:56 +0800

    增量更新 20190403

--- src/com/theredpixelteam/torch/cocoabean/reflection/ReflectionValueHandle.java
+++ src/com/theredpixelteam/torch/cocoabean/reflection/ReflectionValueHandle.java
@@ -8,6 +8,7 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Objects;
 
 /**
  * Reflection implementation of value-type CocoaBean entity handle.
@@ -15,12 +16,14 @@ import java.lang.reflect.Method;
 public class ReflectionValueHandle extends ValueHandle {
     public ReflectionValueHandle(@Nonnull CocoaBeanElementType type,
                                  @Nonnull String identity,
+                                 @Nonnull Class<?> valueType,
                                  @Nullable Field field,
                                  @Nullable Method getter,
                                  @Nullable Method setter)
     {
         super(type, identity);
 
+        this.valueType = Objects.requireNonNull(valueType, "valueType");
         this.field = field;
         this.getter = getter;
         this.setter = setter;
@@ -37,14 +40,61 @@ public class ReflectionValueHandle extends ValueHandle {
     @Override
     public @Nonnull CocoaBeanElement.ValueAccessor createAccessor(Object instance)
     {
-        return null;
+        return new ReflectionValueAccessor(instance);
     }
 
-    // TODO
+    /**
+     * Reflection implementation of value accessor.
+     */
+    public class ReflectionValueAccessor implements CocoaBeanElement.ValueAccessor
+    {
+        public ReflectionValueAccessor(Object instance)
+        {
+            this.instance = instance;
+        }
+
+        @Nullable
+        @Override
+        public Object getValue()
+        {
+            // TODO
+            return null;
+        }
+
+        @Override
+        public boolean setValue(@Nonnull Object value)
+        {
+            // TODO
+            return false;
+        }
+
+        @Override
+        public boolean isModifiable()
+        {
+            return writeable;
+        }
+
+        @Override
+        public boolean isReadable()
+        {
+            return readable;
+        }
+
+        @Nonnull
+        @Override
+        public Class<?> getType()
+        {
+            return valueType;
+        }
+
+        private final Object instance;
+    }
 
     private final boolean readable;
 
     private final boolean writeable;
+
+    private final Class<?> valueType;
 
     private final Field field;
 

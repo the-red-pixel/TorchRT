83058ac22f7aaf5a2f62355ed6d14b76f54940f1
Kumonda221 <kumonda@kucro3.org>
2019-06-25 00:57:08 +0800

    增量更新 20190625

--- src/com/theredpixelteam/cocoabean/CocoaBeanElementType.java
+++
@@ -1,6 +0,0 @@
-package com.theredpixelteam.cocoabean;
-
-public enum CocoaBeanElementType {
-    VALUE,
-    TRIGGER
-}
--- src/com/theredpixelteam/torch/Torch.java
+++ src/com/theredpixelteam/torch/Torch.java
@@ -1,8 +1,6 @@
 package com.theredpixelteam.torch;
 
 import com.google.inject.Inject;
-import com.theredpixelteam.redtea.util.Optional;
-import com.theredpixelteam.torch.cocoabean.TorchCocoaBeanService;
 import org.slf4j.Logger;
 import org.spongepowered.api.event.Listener;
 import org.spongepowered.api.event.game.state.GameConstructionEvent;
@@ -20,46 +18,9 @@ public class Torch {
         logger.info("Starting server with Torch Runtime Subsystem");
     }
 
-    /**
-     * Get the Torch CocoaBean service instance.
-     *
-     * @return {@link TorchCocoaBeanService} instance, if enabled
-     */
-    public static Optional<TorchCocoaBeanService> getCocoaBeanService()
-    {
-        return Optional.ofNullable(cocoaBeanService);
-    }
-
-    /**
-     * Get the Torch CocoaBean service instance directly and throw
-     * {@link UnsupportedOperationException} when the service
-     * is not available.
-     *
-     * @throws UnsupportedOperationException If not available
-     * @return {@link TorchCocoaBeanService} instance
-     */
-    public static TorchCocoaBeanService requireCocoaBeanService()
-        throws UnsupportedOperationException
-    {
-        return getCocoaBeanService()
-                .orElseThrow(() -> new UnsupportedOperationException("CocoaBean service not available"));
-    }
-
-    /**
-     * Whether the CocoaBean service is enabled.
-     *
-     * @return Result
-     */
-    public static boolean isCocoaBeanEnabled()
-    {
-        return cocoaBeanService != null;
-    }
-
     public static final String VERSION = "1.0b";
 
     public static final String NAME = "TorchRuntime";
-
-    private static TorchCocoaBeanService cocoaBeanService;
 
     @Inject
     private Logger logger;
